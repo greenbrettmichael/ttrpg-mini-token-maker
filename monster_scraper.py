081680ffc384b2a4d808425f92a5c96f62ccd79e
Brett-Michael Green <me@brettmichaelgreen.com>
2019-12-04 00:43:58 -0500

    follow monster links

--- monster_scraper.py
+++ monster_scraper.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python
 import random
 import requests
+from time import sleep
 from bs4 import BeautifulSoup
 
 class UserAgent:
@@ -26,8 +28,32 @@ def generate_user_agent_header():
     )
     return headers
 
-monster_table_page = requests.get("https://www.aidedd.org/dnd-filters/monsters.php", headers=generate_user_agent_header())
-if monster_table_page.status_code != requests.codes.ok:
-    print_request_error(monster_table_page)
-monster_table_parser = BeautifulSoup(monster_table_page.content, 'html.parser')
-print(monster_table_parser.prettify())
+def aiedd_monster_crawl():
+    try:
+        monster_table_page = requests.get("https://www.aidedd.org/dnd-filters/monsters.php", headers=generate_user_agent_header(), timeout=5)
+        if monster_table_page.status_code != requests.codes.ok:
+            print_request_error(monster_table_page)
+        monster_table_parser = BeautifulSoup(monster_table_page.content, 'html.parser')
+        monster_table_page_links = monster_table_parser.select("td a")
+        for link_tag in monster_table_page_links:
+            sleep(0.2) # be nice and don't CDoS
+            monster_link_href = link_tag.get("href")
+            try:
+                monster_page = requests.get(monster_link_href, headers=generate_user_agent_header(), timeout=5)
+                if monster_table_page.status_code != requests.codes.ok:
+                    print_request_error(monster_table_page)
+                    continue
+                monster_page_parser = BeautifulSoup(monster_page.content, 'html.parser')
+                print(monster_page_parser.prettify())
+            except requests.Timeout as e:
+                print(monster_link_href + " timed out")
+    except requests.Timeout as e:
+        print("https://www.aidedd.org/dnd-filters/monsters.php timed out")
+        
+        
+
+        
+
+
+aiedd_monster_crawl()
+
