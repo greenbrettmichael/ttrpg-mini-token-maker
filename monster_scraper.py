07315ab1382f1c8d613bb3be0c26e94876778ab9
Brett-Michael Green <me@brettmichaelgreen.com>
2019-12-04 02:27:28 -0500

    picture checkpoint

--- monster_scraper.py
+++ monster_scraper.py
@@ -2,6 +2,7 @@
 import random
 import requests
 import os
+import shutil
 from time import sleep
 from bs4 import BeautifulSoup
 from bs4.element import Comment
@@ -19,7 +20,7 @@ class UserAgent:
         return [table.find('td').text for table in tables]
 
 def print_request_error(page):
-    print("Error Requesting " + page.url + " ,HTTP code: " + page.status_code)
+    print("Error Requesting " + page.url + " ,HTTP code: " + str(page.status_code))
 
 def generate_user_agent_header():
     headers = requests.utils.default_headers()
@@ -44,8 +45,9 @@ def aiedd_monster_crawl(output_folder):
             print_request_error(monster_table_page)
         monster_table_parser = BeautifulSoup(monster_table_page.content, 'html.parser')
         monster_table_page_links = monster_table_parser.select("td a")
+        random.shuffle(monster_table_page_links)
         for link_tag in monster_table_page_links:
-            sleep(0.2) # be nice and don't CDoS
+            sleep(random.uniform(.5, 1.5)) # be nice and don't CDoS
             monster_link_href = link_tag.get("href")
             monster_name_param_split = monster_link_href.rsplit('vo=', 1)
             if len(monster_name_param_split) != 2:
@@ -57,17 +59,29 @@ def aiedd_monster_crawl(output_folder):
             os.makedirs(monster_folder)
             try:
                 monster_page = requests.get(monster_link_href, headers=generate_user_agent_header(), timeout=5)
-                if monster_table_page.status_code != requests.codes.ok:
-                    print_request_error(monster_table_page)
+                if monster_page.status_code != requests.codes.ok:
+                    print_request_error(monster_page)
                     continue
                 monster_page_parser = BeautifulSoup(monster_page.content, 'html.parser')
-                monster_description_parse = monster_page_parser.find_all(text=True)
-                monster_description_visible = filter(tag_visible, monster_description_parse)
-                monster_description_text = u" ".join(t.strip() for t in monster_description_visible)
-                monster_description_file = open(monster_folder + "/description.txt","w")
-                monster_description_file.write(monster_description_text)
-                monster_description_file.close()
-                exit()
+                monster_html_file = open(monster_folder + "/description.html","w")
+                monster_html_file.write(monster_page_parser.prettify(formatter="html"))
+                monster_html_file.close()
+                monster_image_list = monster_page_parser.select("div.picture img, div.picture amp-img")
+                if len(monster_image_list) > 1:
+                    print("too many images, saving first")
+                elif not monster_image_list:
+                    print(monster_name + " has no images")
+                    continue
+                monster_image_src = monster_image_list[0].get("src")
+                try:
+                    monster_image_page = requests.get(monster_image_src, stream=True, timeout=5)
+                    if monster_image_page.status_code != requests.codes.ok:
+                        print_request_error(monster_image_page)
+                        continue
+                    with open(monster_folder + "/image.png", 'wb') as out_file:
+                        shutil.copyfileobj(monster_image_page.raw, out_file)
+                except requests.Timeout as e:
+                    print(monster_image_src + "timed out")
             except requests.Timeout as e:
                 print(monster_link_href + " timed out")
     except requests.Timeout as e:
