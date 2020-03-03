b4d89c461059df0c301eca03fe0418cc46b1ca4b
brettmichaelgreen <personal@brettmichaelgreen.com>
2020-03-03 01:39:08 -0500

    scale images for display

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+function scaleImage(img, width, height) {
+    let canvas = document.createElement("canvas");
+    let ctx = canvas.getContext("2d");
+    canvas.height = height;
+    canvas.width = width;
+    ctx.drawImage(img, 0, 0,  img.width, img.height, 0,0, width, height);
+    return canvas;
+}
+
 function addImage() {
+    const canvas = scaleImage(this, 128, 128);
+    let img = document.createElement('img');
+    img.src = canvas.toDataURL("image/jpeg");
+    img.name = this.name;
+    img.alt = this.alt;
     let imageList = document.getElementById('imageList');
     let imageFile = document.createElement("li");
     let imageLabel = document.createElement("label");
-    imageLabel.setAttribute("for",this.name);
-    imageLabel.innerText  = this.name;
+    imageLabel.setAttribute("for", img.name);
+    imageLabel.innerText = img.name;
     imageFile.appendChild(imageLabel);
+    imageFile.appendChild(img);
     imageFile.appendChild(this);
     imageList.appendChild(imageFile);
 }
@@ -16,6 +31,8 @@ function loadImages() {
         const basename = file.name.split('.')[file.name.split('.').length - 2];
         img.alt = basename;
         img.name = basename;
+        img.class = "real-image"
+        img.style = "display: none;";
         img.onload = addImage;
     });
 }
