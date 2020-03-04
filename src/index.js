272bf6f7e4abf04266c3770181bc6296f517d50f
brettmichaelgreen <personal@brettmichaelgreen.com>
2020-03-04 00:34:58 -0500

    checkpoint saving uploaded images to pdf

--- src/index.js
+++ src/index.js
@@ -7,37 +7,41 @@ function scaleImage(img, width, height) {
     return canvas;
 }
 
-function addImage() {
-    const canvas = scaleImage(this, 128, 128);
-    let img = document.createElement('img');
-    img.src = canvas.toDataURL("image/jpeg");
-    img.name = this.name;
-    img.alt = this.alt;
-    let imageList = document.getElementById('imageList');
-    let imageFile = document.createElement("li");
-    let imageLabel = document.createElement("label");
-    imageLabel.setAttribute("for", img.name);
-    imageLabel.innerText = img.name;
-    imageFile.appendChild(imageLabel);
-    imageFile.appendChild(img);
-    imageFile.appendChild(this);
-    imageList.appendChild(imageFile);
+function addImage(img) {
+    const canvas = scaleImage(img, 128, 128);
+    let token = document.createElement('img');
+    token.src = canvas.toDataURL("image/jpeg");
+    token.name = img.name;
+    token.alt = img.alt;
+    return token;
 }
 
-function loadImages() {
-    [...this.files].map(file => {
+function loadImage(file) {
+    return new Promise((resolve, reject) => {
         let img = document.createElement('img');
+        img.name = file.name;
+        img.alt = file.name;
+        img.onload = () => resolve(addImage(img));
+        img.onerror = reject;
         img.src = URL.createObjectURL(file);
-        const basename = file.name.split('.')[file.name.split('.').length - 2];
-        img.alt = basename;
-        img.name = basename;
-        img.class = "real-image"
-        img.style = "display: none;";
-        img.onload = addImage;
     });
 }
 
+async function loadImages(uploader, doc) {
+    imgLoaders = [...uploader.files].map(file => {
+        return loadImage(file).then(img => {
+            doc.addImage(img.src, 'jpeg', 0, 0);
+        });
+    });
+    await Promise.all(imgLoaders);
+    doc.save('tokens.pdf');
+}
+
+function createPDF() {
+    let doc = new jsPDF('p', 'mm')
+    loadImages(this, doc);
+}
+
 window.addEventListener('load', function () {
-    document.querySelector('#fileUpload').addEventListener('change', loadImages);
-   // document.querySelector('#convert').addEventListener('click', )
+    document.querySelector('#fileUpload').addEventListener('change', createPDF);
 });
