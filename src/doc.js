cfde215ede7b968d7900151ae987d9d452dfebdb
Brett-Michael Green <me@brettmichaelgreen.com>
2020-03-21 22:11:37 -0400

    use margin to seperate tokens

--- src/doc.js
+++ src/doc.js
@@ -1,20 +1,21 @@
 class Doc {
     #a4Width = 8.27;
     #a4Height = 11.69;
+    #margin = 0.125;
     #wIters;
     #hIters;
     #wIter = 0;
     #hIter = 0;
     #pdf = new jsPDF('p', 'in');;
     constructor(tokenSize) {
-        this.tokenSize = tokenSize;
-        this.#wIters = Math.floor(this.#a4Width / this.tokenSize);
-        this.#hIters = Math.floor(this.#a4Height / this.tokenSize);
+        this.tokenSize = Number(tokenSize);
+        this.#wIters = Math.floor((this.#a4Width - 2*this.#margin) / this.tokenSize) - 1;
+        this.#hIters = Math.floor((this.#a4Height - 2*this.#margin) / this.tokenSize) - 1;
     }
 
     addImage(src) {
-        const wStart = this.#wIter * this.tokenSize;
-        const hStart = this.#hIter * this.tokenSize;
+        const wStart = (this.#wIter * (this.tokenSize + this.#margin)) + this.#margin;
+        const hStart = (this.#hIter * (this.tokenSize + this.#margin)) + this.#margin;
         this.#pdf.addImage(src, 'jpeg', wStart, hStart);
         this.#wIter++;
         if(this.#wIter > this.#wIters) {
